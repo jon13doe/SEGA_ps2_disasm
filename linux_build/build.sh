9094696baa247d33d4b701a3e5b96c1d8cbf1184
Zuccace <ilja.sara@kahvipannu.com>
2017-12-14 22:51:53 +0200

    Pushed new version of the build.sh.

--- linux_build/build.sh
+++ linux_build/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-version="0.1.1"
+version="0.2.1b"
 
 this="${0##*/}"
 
-usage="USAGE: ${this} <assembly file> <output> [output] [... [outputN]]
+usage="USAGE: ${this} [--switches] <assembly file> <output> [output] [... [outputN]]
        ${this} --help"
 
 help="$this for disassembled SEGA Mega Drive / Genesis Phantasy Star games.
@@ -17,20 +17,28 @@ Major dependencies are
     - gawk 4.1.1 or newer for inline patching
     - gcc only for compiling the p2bin
     - BSDiff, Xdelta, bdiff, ips.py for support of patch file creation
+    - md5sum, sha1sum, sha244sum, sha256sum, sha384sum and sha512sum to be able to create checksums of the compiled binary 
 
 Note that this script may reside anywhere in the filesystem.
 Also running this script outside of the directory where the assembly is allowed.
 
 $usage
 
-Where output may be *.bin *.bsdiff *.xdelta *.bdiff ...
+Where output may be *.bin *.bsdiff *.xdelta *.bdiff *.md5 *.sha1 *.sha244 *.sha256 *.sha384 *.sha512 ...
 
 Switches:
+
+    Location of the Macro assembler AS.
+    --asl <asl executable>
+
     Location of p2bin.
     --p2bin <p2bin executable>
 
     See README.adoc 'Known limitations' -section for this.
     --fixheader <fixheader executable>
+
+    Specify locations of helper programs
+    --bsdiff --xdelta --bdiff --ips-py
 
     Does not delete temporary files.
     --keep-temp
@@ -72,30 +80,50 @@ check_dep() {
     fi
 }
 
-ask_install() {
+find_one() {
+    one="$(find "${includedir}" -type f -iname "$1")"
+    if  [ "$(echo "$one" | wc -l)" -gt 1 ]
+    then
+        echo -n "$one"
+        return 1
+    elif [ "$one" ]
+    then
+        echo -n "$one"
+    else
+        return 127
+    fi
+}
+
+ask_download() {
+    # $1 = dest, $2 = uri
     bn="${1##*/}"
-    msg "Didn't found '${bn}'.\nProceed with downloading ${bn} and installing it into '${includedir}'? [Y/n]"
+    dn="${1%/*}"
+    msg "Didn't found '${bn}'.\nProceed with downloading ${bn} and installing it into '${dn}'? [Y/n]"
     read answer && case "$answer" in
         [Yy]|[Yy][Ee][Ss])
-            wget -O "${includedir}/${bn}" "$2" || { warn "Downloading of "$bn" failed."; return 1; }
+            wget -nv -O "$1" "$2" || { warn "Downloading of "$bn" failed."; return 1; }
         ;;
         [Nn][Oo]?)
-            msg "Skipped ips patch creation by user request."
+            msg "Skipped installing of $bn by user request."
             return 1
         ;;
         *)
-            [ "$answer" == "" ] && wget -o "${includedir}/${bn}" "$2" || { warn "Downloading of "$bn" failed."; return 1; }
+            [ "$answer" == "" ] && wget -O "$1" "$2" || { warn "Downloading of "$bn" failed."; return 1; }
         ;;
     esac || return 1
 }
 
 # Path patching using awk.
 # While we're at it replace paths with absolute ones.
-# Why? I didn't find any way to tell asl
-# from which diretory to search for includes.
+# Why? I didn't find any simmple way to tell asl
+# from which diretory to search for includes,
+# because there are many of them at leas in case of PSII
+# and I just cannot give a root directory where all
+# the directories reside where the includes are.
 path_patch() {
     # We are using gawk. Would maybe be better to use POSIX awk instead...
-    gawk -i inplace -v "includedir=${includedir}" '{if (/b?include\s+"/) { sub("\"","\"" includedir "/"); gsub("\\\\","/") } print }' "$@"
+    # Although gsub is gawk extension. :(
+    gawk -v "includedir=${includedir}" '{if (/b?include\s+"/) { sub("\"","\"" includedir "/"); gsub("\\\\","/") } print }' "$@"
 }
 
 # Creates the final binary into the temp directory
@@ -112,14 +140,56 @@ build_bin() {
     fi
 }
 
+setup_helper() {
+    # $1 = destination, $2 = source file, $3 = source uri
+    if helper_compiler="$(which gcc 2> /dev/null)" || helper_compiler="$(which clang 2> /dev/null)"
+    then
+        helper_source="$(find_one "$2")"
+        status="$?"
+        if [ "$status" -eq 0 ]
+        then
+            if "$helper_compiler" -O3 -w -o "${helperdir}/$1" "$helper_source"
+            then
+                msg "$1 compiled..."
+            else
+                warn "Compiling $1 failed."
+                return 1
+            fi
+        elif [ "$status" -eq 1 ]
+        then # Multiple choices!
+            warn "Multiple sources for ${1} found:\n${helper_source}"
+            return 1
+        elif [ "$3" ]
+        then                                                      # Recursion!
+            ask_download "${helperdir}/${2}" "$3" && setup_helper "$1" "$2" || return 1
+        else
+            warn "Didn't find '${2}', the source file to compile ${1}."
+            return 127
+        fi
+    else
+        warn "Couldn't find gcc or clang. Cannot compile ${2}."
+        return 127
+    fi
+}
+
 # Be careful. fix_bin_header is this function and fixheader is the binary.
 fix_bin_header() {
     # Choose if we want to fix the header.
-    if [ "$fixheader" ] && [ ! "$nohfix" ]
+    if [ -x "${fixheader:=${helperdir}/fixheader}" ]
     then
         "$fixheader" "$1" && msg "Fixed the header..." || warn "Header fixing failed!"
     else
-         warn "Binary header left unfixed."
+         setup_helper fixheader{,.cpp} "https://raw.githubusercontent.com/sonicretro/s2disasm/ab771f939679b27398d3bd45c3c390508b0d0a33/build_source/fixheader.cpp" \
+             && fix_bin_header "$1" \
+             || { rm -r "$workdir"; errexit 1 "Unable to set up the fixheader helper. You might need to do it by yourself. Aborting..."; }
+    fi
+}
+
+setup_p2bin() {
+    # Find source and compile the p2bin program if needed.
+    if [ ! -e "${p2bin:=${helperdir}/p2bin}" ]
+    then
+        setup_helper p2bin '*p2bin.c' || { rm -r "${workdir}"; errexit "Unable to create p2bin. Aborting..."; }
     fi
 }
 
@@ -145,12 +215,17 @@ create_bdelta() {
 create_ips() {
     if check_dep python3
     then
-        if ! ips_py="$(find "${includedir}" -type f -name 'ips.py' 2> /dev/null)"
+        [ "$ips_py" ] || ips_py="$(find_one 'ips.py')"
+        status="$?"
+        if [ "$status" -eq 0 ]
         then
-            ask_install ips.py https://raw.githubusercontent.com/fbeaudet/ips.py/1fcf07a03111bac8ede9493f414765d8e4e32cfe/ips.py && \
-            ips_py="${includedir}/ips.py" && python3 "$ips_py" create "$1" "$2" "$3" &&  msg "ips created to '$3'..." || warn "ips failed."
+            python3 "$ips_py" create "$1" "$2" "$3" > /dev/null &&  msg "ips created to '$3'..." || warn "ips failed"
+        elif [ "$status" -eq 127 ]
+        then
+            ask_download "${helperdir}/ips.py" https://raw.githubusercontent.com/fbeaudet/ips.py/1fcf07a03111bac8ede9493f414765d8e4e32cfe/ips.py && \
+            ips_py="${helperdir}/ips.py" && python3 "$ips_py" create "$1" "$2" "$3" > /dev/null &&  msg "ips created to '$3'..." || warn "ips failed."
         else
-            python3 "$ips_py" create "$1" "$2" "$3" &&  msg "ips created to '$3'..." || warn "ips failed"
+             warn "ips Creation failed. There seems to be many possible sources for ips.py..."
         fi
     else
         warn "python3 missing. Skipping ips patch creation."
@@ -161,6 +236,11 @@ create_ips() {
 while [ "${1:0:1}" = "-" ]
 do
     case "$1" in
+        --asl)
+            [ "$2" ] && check_dep "$2" die
+            asl="$2"
+            shift
+        ;;
         --p2bin)
             [ "$2" ] && check_dep "$2" die
             p2bin="$2"
@@ -195,10 +275,6 @@ do
             [ -e "$2" ] && orig_bin="$2" || errexit "No such file '${2}'."
             shift
         ;;
-        # Undocumented. Will use later.
-        --no-header-fix)
-            nohfix=1
-        ;;
         --keep-temp)
             keeptemp=1
         ;;
@@ -232,6 +308,8 @@ workdir="$(mktemp -td AS_tmp_XXXXXX)"
 # Set include dir to the directory where the assembly file is in.
 includedir="$(readlink -f "$1")"
 includedir="${includedir%/*}"
+helperdir="${includedir}/linux_helpers"
+[ ! -e "$helperdir" ] && mkdir "$helperdir"
 
 temp_log="${workdir}/${ASlog##*/}"
 temp_p="${workdir}/out.p"
@@ -242,36 +320,20 @@ temp_bin="${workdir}/out.bin"
 # So by default it's the same as the main assembly file with extension changed to 'bin'.
 : ${orig_bin:="${1%.*}original.bin"}
 
-# Copy files to our temporary directory.
-# Should just straight redirect (g)awk output there... TODO
-cp "$1" "$workdir"
-
 baseasm="${1##*/}"
 base="${baseasm%.*}"
 asmfile="${workdir}/${baseasm}"
 
-# Patch and compile the assembly.
-path_patch "$asmfile" && msg "Path patch applied..." || errexit "Patching failed. '$tempdir' -directory is left undeleted."
-"${asl:="asl"}" -xx -c -A -l -shareout "$temp_h" -o "$temp_p" "$asmfile" > "$temp_log" 2>&1 \
-    && msg "Source compiled..." \
-    || errexit "Source compiling failed. Temporary files are left intact inside '${workdir}' -directory."
+setup_p2bin
 
-# Find source and compile the p2bin program if needed.
-if ! [ -e "${p2bin:=${includedir}/p2bin}" ]
+# Patch and compile the assembly.
+path_patch "$1" > "$asmfile" && msg "Path patch applied..." || errexit "Patching failed. '$tempdir' -directory is left undeleted."
+check_dep "${asl:="asl"}" die
+if "${asl}" -xx -c -A -l -shareout "$temp_h" -o "$temp_p" "$asmfile" > "$temp_log" 2>&1
 then
-    p2bin_source="$(find "$includedir" -name '*p2bin.c')"
-    if [ "$(wc -l <<< "$p2bin_source")" -eq "1" ]
-    then
-        if gcc -O3 -w -o "$p2bin" "$p2bin_source"
-        then
-            msg "p2bin compiled..."
-        else
-            rm -r "$workdir"
-            warn "Compiling p2bin failed."
-            [ -e "$p2bin" ] && rm "$p2bin"
-            errexit "Aborting..."
-        fi
-    fi
+    msg "Source compiled..."
+else
+    errexit "Source compiling failed. Temporary files are left intact inside '${workdir}' -directory."
 fi
 
 # Combine all the files to form the binary
@@ -283,7 +345,7 @@ shift
 
 # Create sums:
 msg "Checksums for the binary: "
-for hash in md5 sha{1,244,256,384,512}
+for hash in md5 sha{1,224,256,384,512}
 do
     if check_dep "${hash}sum"
     then
@@ -300,13 +362,18 @@ do
             out_bin="$1"
         ;;
         bsdiff|xdelta|bdiff|ips)
-            "create_${ext}" "$orig_bin" "$temp_bin" "$1"
+            if [ -e "$orig_bin" ]
+            then
+                "create_${ext}" "$orig_bin" "$temp_bin" "$1"
+            else
+                warn "Can't create $ext patch without a binary to compare to. Maybe use '--orig-bin'?"
+            fi
         ;;
-        md5|sha1|sha244|sha256|sha384|sha512)
+        md5|sha1|sha224|sha256|sha384|sha512)
             if [ -f "${workdir}/${ext}" ]
             then
                 echo "$(cat "${workdir}/${ext}") *${base}.bin" > "$1"
-                msg "${ext} checksum created to '$1'..."
+                msg "${ext} checksum of the binary created to '$1'..."
             else
                 warn "${ext} failed. Maybe you're missing ${ext} command line utility?" 
             fi
